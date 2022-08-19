54d7d2734d67d10254abe6748a82fe1c7f53b13b
mohan <sahumohan338@gmail.com>
2022-08-19 00:26:16 -0700

    [mohan]add: User valid Mobile

--- UserRegistration/TestUser/Valid.cs
+++ UserRegistration/TestUser/Valid.cs
@@ -26,7 +26,13 @@ namespace TestUser
         {
             User valid = new User();
             bool actual = valid.EmailValidation("abc.xyz@bl.co.in");
-
+           Assert.IsTrue(actual);
+        }
+        [TestMethod]
+        public void User_Valid_Mobile()
+        {
+            User valid = new User();
+            bool actual = valid.MobileValidation("91 7898625487");
             Assert.IsTrue(actual);
         }
     }
--- UserRegistration/UserRegistration/User.cs
+++ UserRegistration/UserRegistration/User.cs
@@ -11,7 +11,7 @@ namespace UserRegistration
     {
         public static string REGEX_PATTERN = "^[A-Z]{1}[A-Za-z]{2,}$";
         public static string EMAIL_REGEX_PATTERN = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
-
+        public static string MOBILE_REGEX_PATTERN = "^[91]{2}[' ']{1}[7898625487]{10}$";
         public bool FNameValidation(string name)
         {
             try{
@@ -40,6 +40,18 @@ namespace UserRegistration
             try
             {
                 return Regex.IsMatch(email, EMAIL_REGEX_PATTERN);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public bool MobileValidation(string mobile)
+        {
+            try
+            {
+                return Regex.IsMatch(mobile, MOBILE_REGEX_PATTERN);
 
             }
             catch (Exception ex)
